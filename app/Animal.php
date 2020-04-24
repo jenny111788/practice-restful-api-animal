368161ac35c6858ca256926b274a909d7a5853d9
JENNY <>
2020-04-24 16:26:27 +0800

    [練習Day6]-實作資料庫以及新建動物的方法

--- app/Animal.php
+++ app/Animal.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Animal extends Model
 {
     //
+    protected $fillable = [
+        'type_id',
+        'name',
+        'birthday',
+        'area',
+        'fix',
+        'description',
+        'personality',
+    ];
 }
