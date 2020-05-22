f5eca24ef7569f0c5bdb7b794283c67311860d5b
JENNY <>
2020-05-22 17:54:48 +0800

    [SQL檔案]-新增 Passport 套件產生的資料表和欄位

--- api_doc/sql_bk/animal.sql
+++ api_doc/sql_bk/animal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost
--- 產生時間： 2020 年 05 月 14 日 11:51
+-- 產生時間： 2020 年 05 月 22 日 11:42
 -- 伺服器版本： 10.4.11-MariaDB
 -- PHP 版本： 7.2.26
 
@@ -41,6 +41,15 @@ CREATE TABLE `animals` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- 傾印資料表的資料 `animals`
+--
+
+INSERT INTO `animals` (`id`, `type_id`, `name`, `birthday`, `area`, `fix`, `description`, `personality`, `created_at`, `updated_at`) VALUES
+(1, 1, '大黑', '2020-01-01', '高雄', 1, '非常可愛，非常溫和的一隻狗', '認養她陪伴一輩子', '2020-05-04 16:35:41', '2020-05-04 16:35:41'),
+(2, 1, '養樂多多多', '2020-01-01', '高雄', 1, '非常可愛，非常溫和的一隻狗', '認養她陪伴一輩子', '2020-05-04 16:39:14', '2020-05-12 17:02:00'),
+(3, 1, '大黑3', '2020-01-01', '高雄', 1, '非常可愛，非常溫和的一隻狗', '認養她陪伴一輩子', '2020-05-04 16:39:21', '2020-05-04 16:39:21');
+
 -- --------------------------------------------------------
 
 --
@@ -61,7 +70,105 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2020_04_14_083627_create_animals_table', 1),
-(4, '2020_05_14_072736_create_types_table', 2);
+(4, '2020_05_14_072736_create_types_table', 2),
+(5, '2016_06_01_000001_create_oauth_auth_codes_table', 3),
+(6, '2016_06_01_000002_create_oauth_access_tokens_table', 3),
+(7, '2016_06_01_000003_create_oauth_refresh_tokens_table', 3),
+(8, '2016_06_01_000004_create_oauth_clients_table', 3),
+(9, '2016_06_01_000005_create_oauth_personal_access_clients_table', 3);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `oauth_access_tokens`
+--
+
+CREATE TABLE `oauth_access_tokens` (
+  `id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `client_id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `scopes` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `revoked` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `expires_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `oauth_auth_codes`
+--
+
+CREATE TABLE `oauth_auth_codes` (
+  `id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `client_id` int(10) UNSIGNED NOT NULL,
+  `scopes` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `revoked` tinyint(1) NOT NULL,
+  `expires_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `oauth_clients`
+--
+
+CREATE TABLE `oauth_clients` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `secret` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `redirect` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `personal_access_client` tinyint(1) NOT NULL,
+  `password_client` tinyint(1) NOT NULL,
+  `revoked` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `oauth_clients`
+--
+
+INSERT INTO `oauth_clients` (`id`, `user_id`, `name`, `secret`, `redirect`, `personal_access_client`, `password_client`, `revoked`, `created_at`, `updated_at`) VALUES
+(1, NULL, 'Laravel Personal Access Client', 'jHGgR4y3zx6SpzywZnCrFwoZ4OMVQPTsjh45Lg5T', 'http://localhost', 1, 0, 0, '2020-05-22 01:24:00', '2020-05-22 01:24:00'),
+(2, NULL, 'Laravel Password Grant Client', 'XJ9Y9vK6wEo7NDqe4wkJHF7XZpQhJTH88LFsBMn2', 'http://localhost', 0, 1, 0, '2020-05-22 01:24:00', '2020-05-22 01:24:00');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `oauth_personal_access_clients`
+--
+
+CREATE TABLE `oauth_personal_access_clients` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `client_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `oauth_personal_access_clients`
+--
+
+INSERT INTO `oauth_personal_access_clients` (`id`, `client_id`, `created_at`, `updated_at`) VALUES
+(1, 1, '2020-05-22 01:24:00', '2020-05-22 01:24:00');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `oauth_refresh_tokens`
+--
+
+CREATE TABLE `oauth_refresh_tokens` (
+  `id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `access_token_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `revoked` tinyint(1) NOT NULL,
+  `expires_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -89,6 +196,14 @@ CREATE TABLE `types` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- 傾印資料表的資料 `types`
+--
+
+INSERT INTO `types` (`id`, `name`, `sort`, `created_at`, `updated_at`) VALUES
+(1, '狗', 1, '2020-05-13 17:45:36', '2020-05-13 17:45:36'),
+(2, '貓', 2, '2020-05-13 17:45:42', '2020-05-13 17:45:42');
+
 -- --------------------------------------------------------
 
 --
@@ -107,6 +222,13 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- 傾印資料表的資料 `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Jenny', 'jenny111788@gmail.com', NULL, '$2y$10$Pnf6Wj5PGbqIF10lCvlBp.fzH26ryhm4oNIML0rC87tn.WMWa7LBW', NULL, '2020-05-21 23:46:22', '2020-05-21 23:46:22');
+
+--
 -- 已傾印資料表的索引
 --
 
@@ -121,6 +243,40 @@ ALTER TABLE `animals`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `oauth_access_tokens`
+--
+ALTER TABLE `oauth_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `oauth_access_tokens_user_id_index` (`user_id`);
+
+--
+-- 資料表索引 `oauth_auth_codes`
+--
+ALTER TABLE `oauth_auth_codes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `oauth_clients`
+--
+ALTER TABLE `oauth_clients`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `oauth_clients_user_id_index` (`user_id`);
+
+--
+-- 資料表索引 `oauth_personal_access_clients`
+--
+ALTER TABLE `oauth_personal_access_clients`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `oauth_personal_access_clients_client_id_index` (`client_id`);
+
+--
+-- 資料表索引 `oauth_refresh_tokens`
+--
+ALTER TABLE `oauth_refresh_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `oauth_refresh_tokens_access_token_id_index` (`access_token_id`);
 
 --
 -- 資料表索引 `password_resets`
@@ -150,25 +306,37 @@ ALTER TABLE `users`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `animals`
 --
 ALTER TABLE `animals`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `oauth_clients`
+--
+ALTER TABLE `oauth_clients`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `oauth_personal_access_clients`
+--
+ALTER TABLE `oauth_personal_access_clients`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `types`
 --
 ALTER TABLE `types`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
